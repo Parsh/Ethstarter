597d0c110b801b003aab0884cf1b3f0d31b3ed18
parsh24 <parsh.cosmos11@gmail.com>
2018-08-22 11:49:59 +0530

    Authoring SC: Registering Approvers

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -4,9 +4,16 @@ contract Campaign{
     
     address public manager;
     uint public minimumContribution;
-    
+    address[] public approvers;
+
     function Campaign(uint _minimum) public {
         manager = msg.sender;
         minimumContribution = _minimum;
     }   
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers.push(msg.sender);
+    }
+        
 }
