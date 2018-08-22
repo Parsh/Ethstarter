7f078077c79819904d2d9b16017e342447fa09ac
parsh24 <parsh.cosmos11@gmail.com>
2018-08-22 12:00:47 +0530

    Authoring SC: Request Structure and modifier

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -2,9 +2,22 @@ pragma solidity ^0.4.17;
 
 contract Campaign{
     
+    struct Request{
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+    Request[] public requests;
+
+    modifier restricted(){
+        require(msg.sender == manager);
+        _;
+    }
 
     function Campaign(uint _minimum) public {
         manager = msg.sender;
