499a5e1b81aa6951e62cfde511535014183ce9d5
parsh24 <parsh.cosmos11@gmail.com>
2018-08-22 20:08:07 +0530

    Testing: Factory and Campaign Deployed

--- tests/Camapaign.test.js
+++ tests/Camapaign.test.js
@@ -19,5 +19,17 @@ describe('Campaign', () => {
         from: accounts[0],
         gas: '1000000'
       });
+
+    await factory.methods.createCampaign('100').send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+
+    campaign = await new web3.eth.Contract(
+      JSON.parse(compiledCampaign.interface),
+      campaignAddress
+    );
   });
 });
