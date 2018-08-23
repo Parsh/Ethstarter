dd96332bb560d25f92b789f1103957c5be716e30
parsh24 <parsh.cosmos11@gmail.com>
2018-08-24 01:37:32 +0530

    New Campaign Factory Instance Deployed

--- deploy.js
+++ deploy.js
@@ -27,7 +27,7 @@ const deploy = async () => {
     });
 
   console.log('Contract Deployed! Contract Address: ', lottery.options.address);
-  //Latest Deployed Campaign Factory Address: 0x39D9ef4edE8ec7A6F12Ca4bD9bD0e5a5c5487c36
+  //Latest Deployed Campaign Factory Address: 0xafd4B30f6e29Af75109794C589f46C417FBF4207
 };
 
 deploy();
