902edc358e83234349189243f7fb0869f5bec1a1
parsh24 <parsh.cosmos11@gmail.com>
2018-08-23 21:30:14 +0530

    Error handling and Loading

--- frontend/src/components/CreateCampaign.js
+++ frontend/src/components/CreateCampaign.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 import web3 from '../ethereum/web3';
+import { runInThisContext } from 'vm';
 
 class CreateCampaign extends Component {
   state = {
-    minimumContribution: ''
+    minimumContribution: '',
+    errorMessage: '',
+    loading: false
   };
 
   onSubmit = async event => {
     event.preventDefault();
-
-    const accounts = await web3.eth.getAccounts();
-    await factory.methods.createCampaing(this.state.minimumContribution).send({
-      from: accounts[0]
+    this.setState({
+      errorMessage: '',
+      loading: true
     });
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0]
+        });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
   };
 
   render() {
+    let errorAlert = null;
+
+    if (this.state.errorMessage) {
+      errorAlert = (
+        <div class="alert alert-danger mt-4 z-depth-2" role="alert">
+          {this.state.errorMessage}
+        </div>
+      );
+    }
+
     return (
       <div className="container" style={{ marginTop: '100px' }}>
         <h1>Create Campaign</h1>
@@ -38,9 +62,23 @@ class CreateCampaign extends Component {
                 this.setState({ minimumContribution: event.target.value })
               }
             />
-            <button type="submit" className="btn btn-lg btn-primary mt-4">
-              Create!
-            </button>
+
+            {this.state.loading ? (
+              <button
+                type="submit"
+                className="btn btn-lg btn-primary mt-4"
+                disabled
+              >
+                <i class="fa fa-refresh fa-spin mr-3"> </i>
+                Creating...
+              </button>
+            ) : (
+              <button type="submit" className="btn btn-lg btn-primary mt-4">
+                Create !
+              </button>
+            )}
+
+            {errorAlert}
           </div>
         </form>
       </div>
