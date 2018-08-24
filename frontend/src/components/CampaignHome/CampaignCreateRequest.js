512aacc8af79993e055e9f8394e1e863d4feda31
parsh24 <parsh.cosmos11@gmail.com>
2018-08-24 22:05:30 +0530

    Create Request: UI + State Init

--- frontend/src/components/CampaignHome/CampaignCreateRequest.js
+++ frontend/src/components/CampaignHome/CampaignCreateRequest.js
@@ -1,10 +1,78 @@
 import React, { Component } from 'react';
 
 class CampaignCreateRequest extends Component {
-  state = {};
+  state = {
+    description: '',
+    value: '',
+    recipientAddress: '',
+    errorMessage: '',
+    loading: false,
+    created: false
+  };
+
+  onSubmit = async event => {
+    event.preventDefault();
+  };
 
   render() {
-    return <div className="container">Create Request</div>;
+    const form = (
+      <form onSubmit={this.onSubmit}>
+        <div className="md-form text-center">
+          <h2>
+            Create a Request <span className="text-muted">#Manager-Only </span>
+          </h2>
+          <div className="md-form mt-5">
+            <input
+              type="text"
+              placeholder="Description"
+              className="form-control form-control-lg mt-4 w-50 m-auto "
+              value={this.state.description}
+              onChange={event =>
+                this.setState({ description: event.target.value })
+              }
+            />
+          </div>
+          <div className="md-form">
+            <input
+              type="text"
+              placeholder="Value in ether"
+              className="form-control form-control-lg mt-4 w-50 m-auto"
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <div className="md-form">
+            <input
+              type="text"
+              placeholder="Recipient's Address"
+              className="form-control form-control-lg mt-4 w-50 m-auto"
+              value={this.state.recipientAddress}
+              onChange={event =>
+                this.setState({ recipientAddress: event.target.value })
+              }
+            />
+          </div>
+          {this.state.loading ? (
+            <div>
+              <button className="btn btn-lg btn-primary mt-4" disabled>
+                <i className="fa fa-refresh fa-spin mr-3"> </i>
+                Contributing...
+              </button>
+            </div>
+          ) : (
+            <button type="submit" className="btn btn-lg btn-primary mt-4">
+              Contribute !
+            </button>
+          )}
+        </div>
+      </form>
+    );
+
+    return (
+      <div className="container" style={{ marginTop: '100px' }}>
+        {form}
+      </div>
+    );
   }
 }
 
