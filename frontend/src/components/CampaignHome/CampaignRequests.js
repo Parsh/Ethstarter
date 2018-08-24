208cd41a313dce64aacf03579c24bc30cba88101
parsh24 <parsh.cosmos11@gmail.com>
2018-08-25 01:25:17 +0530

    Campaign Requests: UI + Mech

--- frontend/src/components/CampaignHome/CampaignRequests.js
+++ frontend/src/components/CampaignHome/CampaignRequests.js
@@ -1,33 +1,107 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
 
 class CampaignRequests extends Component {
   state = {
-    requests: []
+    requests: [],
+    backers: ''
   };
 
+  campaign;
+
   async componentDidMount() {
-    const campaign = Campaign(this.props.match.params.id);
-    const requestsCount = await campaign.methods.getRequestsCount().call();
+    this.campaign = Campaign(this.props.match.params.id);
+    const requestsCount = await this.campaign.methods.getRequestsCount().call();
+    const backersCount = await this.campaign.methods.backersCount().call();
     const requests = await Promise.all(
       Array(parseInt(requestsCount, 10))
         .fill()
         .map((element, index) => {
-          return campaign.methods.requests(index).call();
+          return this.campaign.methods.requests(index).call();
         })
     );
-    this.setState({ requests });
+    this.setState({
+      requests,
+      backers: backersCount
+    });
   }
+
+  onApprove = async index => {
+    const accounts = await web3.eth.getAccounts();
+    await this.campaign.methods.approveRequest(index).send({
+      from: accounts[0]
+    });
+  };
+
+  onFinalize = async index => {
+    const accounts = await web3.eth.getAccounts();
+    await this.campaign.methods.finalizeRequest(index).send({
+      from: accounts[0]
+    });
+  };
+
+  renderRow = () => {
+    return this.state.requests.map((request, index) => {
+      return (
+        <tr key={index}>
+          <th scope="row">{index}</th>
+          <td>{request.description}</td>
+          <td>{web3.utils.fromWei(request.value, 'ether')}</td>
+          <td>{request.recipient}</td>
+          <td>
+            {request.approvalCount}/{this.state.backers}
+          </td>
+          <td>
+            <button
+              className="btn btn-primary"
+              onClick={() => this.onApprove(index)}
+            >
+              Approve
+            </button>
+          </td>
+          <td>
+            <button
+              className="btn btn-mdb-color"
+              onClick={() => this.onFinalize(index)}
+            >
+              Finalize
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  };
+
+  renderTable = () => {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Description</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Recipient</th>
+            <th scope="col">Approval Count</th>
+            <th scope="col">#BackerOnly</th>
+            <th scope="col">#ManagerOnly</th>
+          </tr>
+        </thead>
+        <tbody>{this.renderRow()}</tbody>
+      </table>
+    );
+  };
 
   render() {
     return (
       <div className="container animated fadeIn">
         <Link to={this.props.match.url + '/create-request'}>
-          <button className="btn btn-primary">
+          <button className="btn btn-info">
             Create Request (Manager Only)
           </button>
         </Link>
+        <div>{this.renderTable()}</div>
       </div>
     );
   }
