4efaa63816528710a5b4e3dd2ca9fccfe0accb05
parsh24 <parsh.cosmos11@gmail.com>
2018-08-28 20:58:46 +0530

    Bug Resolved: Other Networks

--- frontend/src/components/CampaignShowcase.js
+++ frontend/src/components/CampaignShowcase.js
@@ -3,14 +3,21 @@ import { ShowCard } from './ui-components/mdb-stateless-components';
 import { Link } from 'react-router-dom';
 
 import factory from '../ethereum/factory';
+import web3 from '../ethereum/web3';
 
 class CampaignShowcase extends Component {
   state = {
     renderCampaigns: null,
+    otherNetwork: null,
     firefoxCORSError: false
   };
 
   async componentDidMount() {
+    const network = await web3.eth.net.getNetworkType();
+    if (network !== 'rinkeby') {
+      this.setState({ otherNetwork: network });
+    }
+
     try {
       const campaigns = await factory.methods.getDeployedCampaigns().call();
 
@@ -27,13 +34,29 @@ class CampaignShowcase extends Component {
   }
 
   render() {
+    let networkError = this.state.otherNetwork ? (
+      <div
+        className="alert alert-danger z-depth-2 text-center animated fadeIn"
+        role="alert"
+        style={{ fontSize: '25px', marginTop: '75px' }}
+      >
+        <div className="mt-3 mb-3">
+          You are on the{' '}
+          <strong>{this.state.otherNetwork.toUpperCase()}</strong> network. At
+          this moment in time, Ethstarter operates only on the{' '}
+          <strong>Rinkeby</strong> network. Therefore, in order to use
+          Ethstarter, please switch the network type in your Metamask extension
+          to Rinkeby.
+        </div>
+      </div>
+    ) : null;
+
     let corsError = this.state.firefoxCORSError ? (
       <div
         className="alert alert-danger z-depth-2 text-center animated fadeIn"
         role="alert"
         style={{ fontSize: '25px', marginTop: '75px' }}
       >
-        {' '}
         <div className="mt-3 mb-3">
           Cross-Origin Request Blocked <strong>@Firefox</strong>.<br />
           We strongly recommend you to use browsers like Chrome, Brave or any
@@ -53,7 +76,7 @@ class CampaignShowcase extends Component {
         </div>
         <h1 className="mt-2">Open Campaigns</h1>
         <div className="mt-5">{this.state.renderCampaigns}</div>
-        {corsError}
+        {corsError} {networkError}
       </div>
     );
   }
